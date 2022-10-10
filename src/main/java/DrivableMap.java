e3b4d72fdf6991d4665086b89036a180740ee1f9
Runyi Li <lirunyi@berkeley.edu>
2022-10-09 23:59:12 -0400

    lab4

--- src/main/java/DrivableMap.java
+++ src/main/java/DrivableMap.java
@@ -27,6 +27,13 @@ class DrivableMap {
      *       in drivable_map, then add the pair to drivable_map.
      *       Return true if the Drivable was added to drivable_map.
      */
+    public boolean addDrivable(String id, Drivable object) {
+        if (!drivable_map.containsKey(id)) {
+            drivable_map.put(id, object);
+            return true;
+        }
+        return false;
+    }
 
 
 
@@ -37,7 +44,14 @@ class DrivableMap {
      * You may want to use drivable_map.keys() or drivable_map.values() to
      * iterate through drivable_map.
      */
-
+    public boolean hasFasterThan(int speed) {
+        for (Drivable value : drivable_map.values()) {
+            if (value.getMaxSpeed() >= speed){
+                return true;
+            }
+        }
+        return false;
+    }
 
 
 
